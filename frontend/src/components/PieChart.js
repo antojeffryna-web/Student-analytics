f254c1e2c2ce4009f8e109c6131ea9922e35b1ff
antojeffryna-web <antojeffryna@gmail.com>
2026-03-22 03:10:08 +0530

    fixed pie chart component bug

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -6,29 +6,28 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 function PieChart({ students }) {
   const safe = students.filter(s => s.status === "Safe").length;
   const risk = students.filter(s => s.status === "At Risk").length;
-  const options = {
-  responsive: true,
-  maintainAspectRatio: false
-};
 
   const data = {
     labels: ["Safe", "At Risk"],
     datasets: [
-      
-  {
-    data: [safe, risk],
-    backgroundColor: ["#4CAF50", "#FF5252"], // green & red
-    borderWidth: 1
-  }
-]
+      {
+        data: [safe, risk],
+        backgroundColor: ["#4CAF50", "#FF5252"],
+        borderWidth: 1
+      }
+    ]
   };
 
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
 
+  return (
+    <div style={{ width: "100%", height: "300px" }}>
+      <Pie data={data} options={options} />
+    </div>
+  );
 }
-return (
-  <div style={{ width: "100%", height: "300px" }}>
-    <Pie data={data} options={options} />
-  </div>
-);
 
 export default PieChart;
